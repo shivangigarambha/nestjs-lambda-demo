f89a834c8a84e4cab23485832c53c0fb6bd1f03b
shivangigarambha <shivangigarambha@gmail.com>
2024-07-08 18:06:24 +0530

    test commit

--- src/s3Trigger.ts
+++ src/s3Trigger.ts
@@ -1,7 +1,7 @@
 import { S3Handler, S3Event } from 'aws-lambda';
 
 export const handler: S3Handler = async (event: S3Event) => {
-  console.log('---- Running s3 trigger -----');
+  console.log('---- Running s3 trigger ------');
   for (const record of event.Records) {
     const bucket = record.s3.bucket.name;
     const key = record.s3.object.key;
